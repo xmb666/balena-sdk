ea9695408fe95fb657a3f5f062730f312465a547
Thodoris Greasidis <thgreasi@gmail.com>
2019-11-19 16:39:46 +0200

    **BREAKING** Drop the apiKey constructor option
    Change-type: major
    See: https://www.flowdock.com/app/rulemotion/resin-frontend/threads/NvDyvCz_lL8Hgc4MK5oFsV71bXy
    See: https://github.com/balena-io/balena-sdk/pull/460
    HQ: https://github.com/balena-io/balena/pull/370
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-sdk.d.ts
+++ typings/balena-sdk.d.ts
@@ -1373,10 +1373,6 @@ declare namespace BalenaSdk {
 
 	interface SdkOptions {
 		apiUrl?: string;
-		/**
-		 * @deprecated Use balena.auth.loginWithToken(apiKey) instead
-		 */
-		apiKey?: string;
 		builderUrl?: string;
 		dataDirectory?: string;
 		isBrowser?: boolean;
